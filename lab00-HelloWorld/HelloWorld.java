88d3eac9a8761a87d22c550f5120ccde939dece3
edwink <ejk0@lehigh.edu>
2014-08-20 20:12:30 +0000

    updated HelloWorld

---
+++ lab00-HelloWorld/HelloWorld.java
@@ -0,0 +1,31 @@
+//////////////////////
+//Edwin Kay
+//Lab 0
+//Hello World Java Program
+//
+//First compile the program
+//      javac HelloWorld.java
+//
+//Then run the program
+//    java HelloWorld
+//
+//
+//  define a class
+public class HelloWorld{
+
+//  add main method
+public static void main(String[] args) {
+    
+// pirnt statement with a box around it
+System.out.println("######################");
+System.out.println("######################");
+System.out.println("#                    #");
+System.out.println("#  Hello World 2014  #");
+System.out.println("#                    #");
+System.out.println("######################");
+System.out.println("######################");
+System.out.println("test");
+
+//    test/testtest
+}
+}
